b6ddf5033e9fb96e3e9deeaabe0ca3ac4b11f913
Karla Falcão <karlapsfalcao@gmail.com>
2024-12-03 20:17:50 -0300

    add day 3 pt2 solution

--- Makefile
+++ Makefile
@@ -15,3 +15,7 @@ run-day2pt2:
 
 run-day3pt1:
 	deno --allow-read day3pt1.js
+
+
+run-day3pt2:
+	deno --allow-read day3pt2.js
--- day3pt2.js
+++ day3pt2.js
@@ -2,15 +2,28 @@ import * as day3input from './day3input.js'
 
 // console.log(day3input)
 const testInput = day3input.input
-// const testInput = 'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'
+// const testInput = 'xmul(2,4)&mul[3,7]!^don\'t()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))'
 console.log(testInput)
-const pattern = /mul\(\d{1,3}\,\d{1,3}\)/g
+const pattern = /mul\(\d{1,3}\,\d{1,3}\)|don't\(\)|do\(\)/g
 
 
 const mullMatchs = testInput.match(pattern)
 console.log(mullMatchs)
-
+let multitDisabled = false
 const multits = mullMatchs.map(mull => {
+	if ( mull === "don't()"){
+		multitDisabled = true
+		return 0
+	}
+
+	if ( mull === "do()"){
+		multitDisabled = false
+		return 0
+	}
+
+	if (multitDisabled) {
+		return 0
+	}
 
 	const mullPattern = /\d{1,3}/g
 	const numbersMatchs = mull.match(mullPattern)
